8356b1c08278f11a721597a3468698c45e7fc8d6
panyingia <panyingia@gmail.com>
2023-07-28 06:16:36 +0800

    2023/7/28 6:15

--- app.js
+++ app.js
@@ -15,9 +15,42 @@ const reviews = [
 // const gallery = document.getElementById('gallery')
 const galleryImg = document.getElementById('gallery-img')
 const thumbs = document.querySelectorAll('.thumb')
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+// 按下小圖時 改變大圖的src屬性
 thumbs.forEach(function (thumb) {
     thumb.addEventListener('click', function (e) {
         const dataImg = e.target.dataset.img
         galleryImg.setAttribute('src', reviews[dataImg].img)
     })
+})
+// 滑鼠滑入小圖時 加上filter樣式 
+// 滑鼠
+thumbs.forEach(function (thumb) {
+    thumb.addEventListener('mouseover', addClassHandler)
+    thumb.addEventListener('mouseleave', removeClassHadler)
+})
+function addClassHandler(e) {
+    e.currentTarget.classList.add('thumb-filter');
+}
+function removeClassHadler(e) {
+    e.currentTarget.classList.remove('thumb-filter');
+}
+
+
+
+let currentItem = 0;
+nextBtn.addEventListener('click', function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    galleryImg.setAttribute('src', reviews[currentItem].img)
+})
+prevBtn.addEventListener('click', function () {
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1
+    }
+    galleryImg.setAttribute('src', reviews[currentItem].img)
 })
